9bd69783e76a8717a1574202028708c1f62f9920
amsKane <kaneams17@gmail.com>
2026-08-19 23:00:48 +0000

    Creation de la structure de base du projet avec spring initializer

---
+++ backend/LocPay/src/main/java/com/locpay/LocPay/LocPayApplication.java
@@ -0,0 +1,13 @@
+package com.locpay.LocPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocPayApplication.class, args);
+	}
+
+}
